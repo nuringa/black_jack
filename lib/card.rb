c71817170e2742fb8ec027dd65032dbf985b088a
Ekaterina Egorova <admin.karima@gmail.com>
2020-03-28 18:12:14 +0300

    constant DECK to card after review

--- lib/card.rb
+++ lib/card.rb
@@ -1,4 +1,6 @@
 class Card
+  DECK = %w[2 3 4 5 6 7 8 9 10 Jack Queen King Ace].product(["\u{2665}",	"\u{2666}", "\u{2663}", "\u{2660}"]).freeze
+
   attr_reader :suite, :rank
 
   def initialize(rank, suite)
--- lib/deck.rb
+++ lib/deck.rb
@@ -1,10 +1,8 @@
 class Deck
-  DECK = %w[2 3 4 5 6 7 8 9 10 Jack Queen King Ace].product(["\u{2665}",	"\u{2666}", "\u{2663}", "\u{2660}"]).freeze
-
   attr_reader :cards
 
   def initialize
-    @cards = DECK.map { |card| Card.new(card[0], card[1]) }.shuffle
+    @cards = Card::DECK.map { |card| Card.new(card[0], card[1]) }.shuffle
   end
 
   def full?
